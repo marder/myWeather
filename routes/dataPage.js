9d6200310927d115fbceaa8b1e8f5cbe40ab1671
marder <marcin.derucki@gmail.com>
2025-04-07 19:05:13 +0200

    07.04.2025

--- routes/dataPage.js
+++ routes/dataPage.js
@@ -8,24 +8,11 @@ router.get("/", async (req, res) => {
     const data = await DhtData.find().limit(24).sort({ readingDate: -1 });
     const last = await DhtData.findOne().sort({ readingDate: -1 });
 
-    let dateLabel = [];
-    let temp = [];
-
-    data.forEach((i) =>
-      dateLabel.push(i["readingDate"].toLocaleTimeString("pl-PL"))
-    );
-
-    data.forEach((i) => temp.push(Math.ceil(i["temperature"])));
-
-    console.log(temp);
-
     res.header("Access-Control-Allow-Origin", "*");
     res.render("index", {
       data: data,
       last: last,
       path: req.url,
-      dateLabel: dateLabel,
-      temp: temp,
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
